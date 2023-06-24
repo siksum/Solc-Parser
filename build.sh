606fbb4c5c706d099053a75e02bcb8e68d1e94ad
Namryeong-Kim <namryeong.kim@dream-academy.io>
2023-06-25 02:41:41 +0900

    update build.sh

--- build.sh
+++ build.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
-serial_numbers = [0, 1, 2, 3, 4, 5, 6, 7,8,9]
 
-current_serial = serial_numbers[-1]
-new_serial = current_serial + 1
+serial_numbers=(0 1 2 3 4 5 6 7 8 9)
 
-major, minor, patch = map(int, current_version.split('.'))
+current_serial=${serial_numbers[-1]}
+new_serial=$((current_serial + 1))
 
-if new_serial == 0:
-    minor += 1
-elif new_serial % 10 == 0:
-    major += 1
+IFS='.' read -ra version_parts <<< "$current_version"
+major=${version_parts[0]}
+minor=${version_parts[1]}
+patch=${version_parts[2]}
 
-new_version = f"{major}.{minor}.{patch}"
-print(f"New version: {new_version}")
+if ((new_serial == 0)); then
+    ((minor += 1))
+elif ((new_serial % 10 == 0)); then
+    ((major += 1))
+fi
 
-setup_file = "setup.py"
-with open(setup_file, "r") as f:
-    lines = f.readlines()
+new_version="$major.$minor.$patch"
+echo "New version: $new_version"
 
-with open(setup_file, "w") as f:
-    for line in lines:
-        if line.startswith("version="):
-            line = f'version="{new_version}",\n'
-        f.write(line)
+setup_file="setup.py"
+while IFS= read -r line; do
+    if [[ $line == version=* ]]; then
+        line="version='$new_version',"
+    fi
+    echo "$line"
+done < "$setup_file" > temp_setup.py
 
-print("setup.py file updated.")
+mv temp_setup.py "$setup_file"
+
+echo "setup.py file updated."
